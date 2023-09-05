5cc16cddf23b7e934ce95d737607a22c67784d4c
Pedro Serpa <pedroserpah@gmail.com>
2023-09-05 11:03:53 -0300

    max-alg

--- max-alg/alg_functions.py
+++ max-alg/alg_functions.py
@@ -1,5 +1,8 @@
 import numpy as np
 from math import cos, pow
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
 
 def f(x):
     return cos(20*x) - abs(x)/2 + pow(x,3)/4
@@ -15,3 +18,41 @@ def get_fitness_from_individual_minimization(x):
     answer = magical_value - f(x)
     fitness_value = answer if answer > 0 else 0
     return float(round(fitness_value, 4))
+
+def print_plot_function_with_point(value: float):
+    x_vals = np.linspace(-2, 2, 300)
+    y_vals = [f(x) for x in x_vals]
+    
+
+    # Create the plot
+    plt.figure(figsize=(8, 6))
+
+    # Plot the function line
+    plt.plot(x_vals, y_vals, label='Function: f(x) cos(20x) - |x|/2 + x^3/4')
+
+    plt.scatter(value, f(value), color='red')
+    plt.scatter(value, f(value), color='red')
+
+    # Annotate the red point with text
+    # Annotate the red point with text
+    plt.annotate('Red Point', xy=(value, f(value)), xytext=(value + 2, f(value) + 2), arrowprops=dict(facecolor='black', shrink=0.05))
+
+    # Write additional text on the plot
+    print(f'Maximization fitness on {value}: {get_fitness_from_individual_maximization(value)}')
+    print(f'minimization fitness on {value}: {get_fitness_from_individual_minimization(value)}')
+
+    # Set plot labels and title
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Function Plot')
+
+    # Display legend
+    plt.legend()
+
+
+
+    
+    # Show the plot
+    plt.grid()
+    plt.show()
+
--- max-alg/main.py
+++ max-alg/main.py
@@ -1,5 +1,5 @@
 from init_functions import *
-from alg_functions import get_fitness_from_individual_maximization, get_fitness_from_individual_minimization, f
+from alg_functions import get_fitness_from_individual_maximization, get_fitness_from_individual_minimization, f, print_plot_function_with_point
 
 if __name__ == "__main__":
     dim, pop, props = get_info_from_base('base1')
@@ -7,7 +7,8 @@ if __name__ == "__main__":
                                              min_value=props["MIN"],
                                              decimals=props["DECIMALS"],
                                              pop_size=props["POP"])
-    initial_population.append(1.8493)
+    value = 1.8493
+    initial_population.append(value)
     initial_fitness_maximization = [get_fitness_from_individual_maximization(x) for x in initial_population]
     initial_fitness_minimization = [get_fitness_from_individual_minimization(x) for x in initial_population]
     initial_fitness_function = [f(x) for x in initial_population]
@@ -22,4 +23,4 @@ if __name__ == "__main__":
     for individual, fitness, function_value in zip(initial_population, initial_fitness_minimization, initial_fitness_function):
         print(f'{individual} -> {fitness} ({function_value})')
 
-    
+    print_plot_function_with_point(value)
